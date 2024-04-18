986bb29b5996540e0a0843d941a0a1cc8803a523
zoness.info <zoness.info@gmail.com>
2024-04-19 00:27:00 +0530

    dashbaord194244-1 updated

--- HomeApp/urls.py
+++ HomeApp/urls.py
@@ -1,9 +1,10 @@
 #from django.contrib import admin
 from django.urls import path
 
-from .views import HomeApp
+from .views import *
 
 urlpatterns = [
-    path('',HomeApp.as_view(),name="HomeApp" )
+    path('',HomeApp.as_view(),name="HomeApp" ),
+    path('all_vehicle',all_vehicle.as_view(),name="all_vehicle"),
 ]
 
--- HomeApp/views.py
+++ HomeApp/views.py
@@ -6,4 +6,8 @@ class HomeApp(View):
     def get(self, request):
         #return HttpResponse("welcome")
         return render(request, 'index.html')
+    
+class all_vehicle(View):
+    def get(self, request):
+        return render(request,'all_vehicle.html')
 # Create your views here.
